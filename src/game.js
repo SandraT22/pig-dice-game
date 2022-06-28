b9a9a2367f2f0e22a80afe84721a33a09d0da3e5
Sandra Tena <sandratena06@gmail.com>
2022-06-28 11:40:08 -0700

    modify turn prototype in game class
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

--- src/game.js
+++ src/game.js
@@ -11,14 +11,71 @@ export class Game {
     if (numberOfDice > 1){
       this.dice2 = new Dice(6);
     }
+
+    this.currentPlayer = 1;
   }
 
   populatePlayers() {
     for (let i = 1; i <= this.maxPlayers; i++) {
-      let player = new Player(i);
-      this.players[player.playerNumber] = new Player(i);
+      let player = new Player(i); //creating new players
+      this.players[player.playerNumber] = new Player(i); //assign player number
     }
   }
 
-  //{Game}.players[{playerNumber}]
+  nextPlayer(){
+    this.lastPlayer = this.currentPlayer; 
+  
+    if(this.currentPlayer < this.maxPlayers) {
+      this.currentPlayer += 1;
+    }
+    else{
+      this.currentPlayer = 1;
+    }
+    // if (isCpuPlayer){
+    //   cpuPlayer();
+    // }
+    return this.currentPlayer;
+  }
+  
+  turn() {
+    if (this.numberOfDice > 1){ //checks for user input of 2 dice
+      if (this.dice1.roll() < 2){ // if you rolled a number equal to 1
+        if (this.dice2.roll() < 2){ //checks if the dice number rolled is greater than 2
+          this.players[currentPlayer].score = 0; // this.players[currentPlayer].score   Sets current players score to 0
+          this.players[currentPlayer].turnScore = 0; //sets score variable back to 0
+          this.nextPlayer(); // calls next player function. 
+        }
+        else{ //if dice 2 is more than 1
+          score = 0; //set score variable back to 0
+          this.nextPlayer(); // call on next player function
+        }
+      }
+      else if(this.dice2.roll() < 2) { //
+        this.players[currentPlayer].turnScore = 0; // sets score variable to 0
+        this.nextPlayer(); // calls the next player function
+      }
+      else {
+        this.players[currentPlayer].turnScore += this.dice1 + this.dice2;
+      }          //  -- end of 2 dice code --  //
+    }
+    else {     // if only 1 dice is inputted 
+      if(this.dice1.roll() < 2) {
+        this.players[currentPlayer].turnScore = 0;
+        this.nextPlayer();
+      }
+      else {
+        this.players[currentPlayer].turnScore += this.dice1;
+      }
+    }
+
+    // if (isCpuPlayer){
+    //   cpuPlayer();
+    // }
+    return this.players[currentPlayer].turnScore;
+    }    
+
 }
+
+  
+
+  //{Game}.players[{playerNumber}]
--- src/player.js
+++ src/player.js
@@ -5,46 +5,46 @@ export class Player {
     this.playerNumber = playerNumber;
   }
 
-  turn() {
-    if (numberOfDice > 1){ //checks for user input of 2 dice
-      console.log("2 dice"); //tests if (numberOfDice > 1) is working
-      if (rollDice() < 2){ // if you rolled a number equal to 1
-        console.log("dice 1 = 1"); // tests if (rollDice() < 2) is working
-        if (roll2Dice() < 2){ //checks if the dice number rolled is greater than 2
-          console.log("dice 2 = 1"); // Tests if (rollDice() < 2) is working
-          players[playerNumber-1] = 0; // Sets current players score to 0
-          score = 0; //sets score variable back to 0
-          nextPlayer(); // calls next player function. 
-        }
-        else{ //if each dice is more than 1
-          console.log("dice 2 = 1"); // Tests if else statement is working
-          score = 0; //set score variable back to 0
-          nextPlayer(); // call on next player function
-        }
-      }
-      else if(roll2Dice() < 2) { //
-        score = 0; // sets score variable to 0
-        nextPlayer(); // calls the next player function
-      }
-      else { //
-        console.log(dice1);
-        console.log(dice2);
-        score += dice1 + dice2;
-      }          //     end of 2 dice code   //
-    }
-    else {     // if only 1 dice is inputted 
-      if(rollDice() < 2) {
-        score = 0;
-        nextPlayer();
-      }
-      else {
-        score += dice1;
-      }
-    }
-    if (isCpuPlayer){
-      cpuPlayer();
-    }
-    return score;
-    }    
+  // turn() {
+  //   if (this.numberOfDice > 1){ //checks for user input of 2 dice
+  //     console.log("2 dice"); //tests if (numberOfDice > 1) is working
+  //     if (this.dice1.roll() < 2){ // if you rolled a number equal to 1
+  //       console.log("dice 1 = 1"); // tests if (rollDice() < 2) is working
+  //       if (this.dice2.roll() < 2){ //checks if the dice number rolled is greater than 2
+  //         console.log("dice 2 = 1"); // Tests if (rollDice() < 2) is working
+  //         this.players[currentPlayer].score = 0; // this.players[currentPlayer].score   Sets current players score to 0
+  //         this.players[currentPlayer].turnScore = 0; //sets score variable back to 0
+  //         this.nextPlayer(); // calls next player function. 
+  //       }
+  //       else{ //if dice 2 is more than 1
+  //         console.log("dice 2 = 1"); // Tests if else statement is working
+  //         score = 0; //set score variable back to 0
+  //         this.nextPlayer(); // call on next player function
+  //       }
+  //     }
+  //     else if(this.dice2.roll() < 2) { //
+  //       this.players[currentPlayer].turnScore = 0; // sets score variable to 0
+  //       this.nextPlayer(); // calls the next player function
+  //     }
+  //     else { //
+  //       console.log(this.dice1);
+  //       console.log(this.dice2);
+  //       this.players[currentPlayer].turnScore += this.dice1 + this.dice2;
+  //     }          //     end of 2 dice code   //
+  //   }
+  //   else {     // if only 1 dice is inputted 
+  //     if(this.dice1.roll() < 2) {
+  //       this.players[currentPlayer].turnScore = 0;
+  //       this.nextPlayer();
+  //     }
+  //     else {
+  //       this.players[currentPlayer].turnScore += this.dice1;
+  //     }
+  //   }
+  //   // if (isCpuPlayer){
+  //   //   cpuPlayer();
+  //   // }
+  //   return this.players[currentPlayer].turnScore;
+  //   }    
 
 }
